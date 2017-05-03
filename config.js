c103245b5789b96cfee04d4a9e55536d1cf86562
cool.coldboy@gmail.com <cool.coldboy@gmail.com>
2017-05-03 17:11:49 +0800

    more

--- config.js
+++ config.js
@@ -3,12 +3,14 @@
 */
 
 //inner net
+// exports.TRACKER_SERVER="10.101.1.165";
 // exports.IMG_HOST="http://10.101.1.165:8097/";//"http://pic.dengnilvyou.com.cn/";
 // exports.JAVA_SERVER="http://10.101.1.165:8096";//"http://10.101.1.165:8096";http://app.dengnilvyou.com.cn
 // exports.LOCAL_HOST = "http://localhost:8089"; //http://101.37.21.172:80
 // exports.DATA_HOST = "10.101.1.163"; //114.55.139.196
 
 //outer net
+exports.TRACKER_SERVER="101.37.22.3";
 exports.IMG_HOST="http://pic.dengnilvyou.com.cn/";
 exports.JAVA_SERVER="http://app.dengnilvyou.com.cn";
 exports.LOCAL_HOST = "http://101.37.21.172:80";
